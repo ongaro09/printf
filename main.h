b9f0405aaaeab155af20eb57c9c1f98594997812
ongaro09 <ongaromark7@gmail.com>
2023-09-18 15:21:33 +0000

    header files

--- main.h
+++ main.h
@@ -8,5 +8,9 @@ char *itoa(int num);
 void print_char(int *char_count, char c);
 void print_string(int *char_count, char *str);
 void print_int(int *char_count, int num);
+int write_char(int c);
+int write_string(const char *str);
+int write_integer(int num);
+int format_specifier(const char **format, va_list args, int *char_count);
 
 #endif
