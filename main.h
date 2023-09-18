33ffac8215883dcf58085acc397e29de009ecd67
ongaro09 <ongaromark7@gmail.com>
2023-09-18 16:42:23 +0000

    header files

--- main.h
+++ main.h
@@ -2,12 +2,13 @@
 #define MY_PRINTF_H
 
 #include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 char *itoa(int num);
-void print_char(int *char_count, char c);
-void print_string(int *char_count, char *str);
-void print_int(int *char_count, int num);
+int print_char(char c);
+int print_string(const char *str);
+int print_int(int num);
 int write_char(int c);
 int write_string(const char *str);
 int write_integer(int num);
