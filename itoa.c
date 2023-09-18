37fc619c9139418d769dfc5acfed27d991086b1e
ongaro09 <ongaromark7@gmail.com>
2023-09-18 14:29:06 +0000

    custom itoa function

--- itoa.c
+++ itoa.c
@@ -7,35 +7,42 @@
  * Return: A pointer to the resulting string.
  */
 
-char *itoa(int num) {
-    int i, j;
-    static char buf[12];
-    int length = 0;
-    int is_negative = 0;
+char *itoa(int num)
+{
+	int i, j;
+	static char buf[12];
 
-    if (num == -2147483648) {
-        return "-2147483648";
-    } else if (num < 0) {
-        is_negative = 1;
-        num = -num;
-    }
+	int length = 0;
+	int is_negative = 0;
 
-    i = length;
-    do {
-        buf[length++] = num % 10 + '0';
-        num /= 10;
-    } while (num > 0);
+	if (num == -2147483648)
+	{
+		return ("-2147483648");
+	}
+	else if (num < 0)
+	{
+		is_negative = 1;
+		num = -num;
+	}
 
-    if (is_negative) {
-        buf[length++] = '-';
-    }
+	i = length;
+	do {
+		buf[length++] = num % 10 + '0';
+		num /= 10;
+	} while (num > 0);
 
-    for (j = i; j < length / 2; j++) {
-        char temp = buf[j];
-        buf[j] = buf[length - j - 1];
-        buf[length - j - 1] = temp;
-    }
+	if (is_negative)
+	{
+		buf[length++] = '-';
+	}
 
-    buf[length] = '\0';
-    return buf;
+	for (j = i; j < length / 2; j++)
+	{
+		char temp = buf[j];
+
+		buf[j] = buf[length - j - 1];
+		buf[length - j - 1] = temp;
+	}
+	buf[length] = '\0';
+	return (buf);
 }
